7cecdef09a5cce1822041c93439c744193eacdd4
Kizito Njoku <nkizito57@gmail.com>
2020-03-18 13:45:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function BoardMember (name, homeState, training) {
   this.training = training;
   
   BoardMember.prototype.veto = () => {
-    return "no, I must disagree"
+    return "No, I must disagree"
   }
   
 }
