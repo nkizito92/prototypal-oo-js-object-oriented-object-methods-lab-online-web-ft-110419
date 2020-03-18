25641220b2c6dcd6d2f84ae24f43b79981d978b1
Kizito Njoku <nkizito57@gmail.com>
2020-03-18 13:49:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,6 @@ function BoardMember (name, homeState, training) {
   }
   BoardMember.prototype.approve = () => "You can do that!"
   BoardMember.prototype.doCharity = () => "I like to help people."
+  BoardMember.prototype.releasePressStatement = () => "You will see great things from Scuber."
+  BoardMember.prototype.sayHi = () => `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 }
